ca49eaac665e2cdb186164d15c5de130a4aff908
SamuelSherpa2020 <samuelsherpa46@gmail.com>
2023-05-11 11:29:26 +0545

    Add project files.

---
+++ JWTTutorial/Auth/Response.cs
@@ -0,0 +1,7 @@
+﻿namespace JWTTutorial.Auth;
+
+public class Response
+{
+    public string? Status { get; set; }
+    public string?  Message{ get; set; }
+}
---
+++ TestAuthentication/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TestAuthentication;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
